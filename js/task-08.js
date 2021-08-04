63ad9b3370d5140e0fe0e2224aef416f48275de2
Vladimir Palamov <vladimir_palamov@mail.ru>
2021-08-04 12:18:07 +0800

    8 зачада(пока что плохо)

--- js/task-08.js
+++ js/task-08.js
@@ -1,42 +1,42 @@
 const inputEl = document.querySelector('input');
-console.log(inputEl);
 
 const renderButEl = document.querySelector('[data-action="render"]');
-console.log(renderButEl);
-const destroybutEl = document.querySelector('[data-action="destroy"]');
-console.log(destroybutEl);
+
+const destroyButEl = document.querySelector('[data-action="destroy"]');
+
+const divEl = document.querySelector("#boxes");
+
 
 let amountEl = 0;
 
-// ФУНКЦИЯ СОЗДАЁТ УКАЗАННОЕ КОЛ-ВО ДИВОВ
 const createBoxes = (amount) => {
     amountEl = amount.currentTarget.value;
     console.log(amountEl);
 };
+inputEl.addEventListener('input', createBoxes);
 
-// ФУНКЦИЯ УДАЛЯЕТ ВСЕ ДИВЫ(ОБНОВЛЯЕТ)
-const destroyBoxes = () => { };
-
-inputEl.addEventListener('input', createBoxes)
-renderButEl.addEventListener('click', () => {
+const protoType = () => {
     const array = [];
     for (let i = 0; i < amountEl; i += 1) {
+        let color = Math.round(Math.random() * 255 + 100);
         array.push(document.createElement('div'));
-        array[i].style.backGroundColor = Object.values(bgcExc);
-    console.log(array[i], array[i].style);
+        array[i].style.width = `${i * 10 + 30}px`;
+        array[i].style.height = `${i * 10 + 30}px`;
+        array[i].style.backgroundColor = `#${color}`;
+    console.log(array[i], color);
+    }
+    divEl.append(...array);
+    return array;
+}
+
+renderButEl.addEventListener('click', protoType);
+
+const destroyBoxes = () => { };
+destroyButEl.addEventListener('click', () => {
+    const divCollection = divEl.querySelectorAll("div");
+    console.log(divEl);
+    console.log(...divCollection);
+    for (const divc of divCollection) {
+        divEl.removeChild(divc);
     }
 });
-destroybutEl.addEventListener('click', () => { });
-let red = Math.round(Math.random() * 255);
-let green = Math.round(Math.random() * 255);
-let blue = Math.round(Math.random() * 255);
-const bgcExc = {
-    red,
-    green,
-    blue,
-};
-console.log(bgcExc);
-console.log(...Object.values(bgcExc));
-console.log(bgcExc.red);
-console.log(bgcExc.green);
-console.log(bgcExc.blue);
